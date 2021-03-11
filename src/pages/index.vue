6a288a21d3b88643a616c2e42d2b042e82822836
zaj <1638962007@qq.com>
2021-03-11 11:20:06 +0800

    Signed-off-by: zaj <1638962007@qq.com>

---
+++ src/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <el-row class="tac">
+
+      <el-col :span="5" offset="5">111111</el-col>
+      <el-col :span="5">111111</el-col>
+      <el-col :span="5">111111</el-col>
+      <el-col :span="4">111111</el-col>
+
+    </el-row>
+     
+
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+
+    }),
+    methods: {
+
+    }
+  }
+</script>
+
+<style>
+  
+</style>
